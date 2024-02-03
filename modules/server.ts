883735e190dda3fbf0cca4107c273f3f0ede006d
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2024-02-03 13:44:13 -0800

    Update static file serving configuration

--- modules/server.ts
+++ modules/server.ts
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(cors());
-// app.use(express.static("public", { maxAge: "1y" }));
+app.use(express.static("public", { maxAge: "1y" }));
 
 app.use(requestLog);
 
